1f0979dfd8e6a0e37ad93d57d692f47a9e061ba4
Robert Chen <robertchen234@gmail.com>
2018-08-19 23:01:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function takeANumber(currentNumber, newName) {
-  for (var i = 0; i < currentNumber.length; i++) {
-  array.push("Welcome " + newName[i] + ". You are " + currentNumber[i] + " in line.");
-  }
-  return array;
-}
+var katzDeli = [];
